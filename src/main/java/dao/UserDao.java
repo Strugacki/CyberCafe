111a0ce00fbe206ceeb42df24ed60247a40617bc
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2015-12-07 18:35:18 +0100

    Added AbstractDao, added some code to UserDao

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,5 +1,18 @@
 package dao;
 
-public class UserDao {
+import java.util.List;
+import com.ug.cyberCafe.domain.User;
+
+public interface UserDao {
+	
+    User getById(int id);
+    
+    void addUser(User user);
+     
+    void deleteEmployeeById(int id);
+     
+    List<User> getAllEmployees();
+ 
+    User getUserById(int id);
 
 }
--- src/main/java/dao/UserDaoImpl.java
+++ src/main/java/dao/UserDaoImpl.java
@@ -1,5 +1,39 @@
 package dao;
 
-public class UserDaoImpl {
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import com.ug.cyberCafe.domain.User;
+
+@Repository("userDao")
+public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao {
+
+	public User getById(int id) {
+		return getByKey(id);
+	}
+
+	public void addUser(User user) {
+		persist(user);
+	}
+
+	public void deleteEmployeeById(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<User> getAllEmployees() {
+		Criteria criteria = createEntityCriteria();
+        return (List<User>) criteria.list();
+	}
+
+	public User getUserById(int id) {
+		Criteria criteria = createEntityCriteria();
+        criteria.add(Restrictions.eq("id", id));
+        return (User) criteria.uniqueResult();
+	}
+
 
 }
