74f974d0c9ff12fa744eb60c8018fe606b8d8a13
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2016-01-04 18:21:00 +0100

    Added method isValidUser to UserDaoImpl

--- src/main/java/com/ug/cyberCafe/dao/UserDaoImpl.java
+++ src/main/java/com/ug/cyberCafe/dao/UserDaoImpl.java
@@ -1,6 +1,11 @@
 package com.ug.cyberCafe.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
@@ -13,9 +18,10 @@ import com.ug.cyberCafe.domain.User;
 
 @Repository("userDao")
 public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao {
-	
+
 	@Autowired
 	private SessionFactory sessionFactory;
+	private DataSource dataSource;
 
 	public void addUser(User user) {
 		sessionFactory.getCurrentSession().save(user);
@@ -24,7 +30,7 @@ public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao {
 	public void deleteUser(User user) {
 		user = (User) sessionFactory.getCurrentSession().get(User.class, user.getIdUser());
 		sessionFactory.getCurrentSession().delete(user);
-		
+
 	}
 
 	public List<User> getAllUsers() {
@@ -35,5 +41,17 @@ public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao {
 		return (User) sessionFactory.getCurrentSession().get(User.class, id);
 	}
 
+	public boolean isValidUser(String login, String password) throws SQLException {
+		String query = "Select count(1) from User where login = ? and password = ?";
+		PreparedStatement pstmt = dataSource.getConnection().prepareStatement(query);
+		pstmt.setString(1, login);
+		pstmt.setString(2, password);
+		ResultSet resultSet = pstmt.executeQuery();
+		if (resultSet.next()) {
+			return (resultSet.getInt(1) > 0);
+		} else {
+			return false;
+		}
+	}
 
 }
