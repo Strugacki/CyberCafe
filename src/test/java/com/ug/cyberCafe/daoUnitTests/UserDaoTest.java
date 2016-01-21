09df58ba5606810e7982038f9849642ad1854a03
Michał <dajminazwe@gmail.com>
2016-01-22 00:35:07 +0100

    UserDaoTest added - with NewsDaoTest methods

---
+++ src/test/java/com/ug/cyberCafe/daoUnitTests/UserDaoTest.java
@@ -0,0 +1,14 @@
+package com.ug.cyberCafe.daoUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
