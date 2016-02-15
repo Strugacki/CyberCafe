061861783bc9355e410cfaf1a27f006b75481d35
Michał Górski <dajminazwe@gmail.com>
2016-02-15 15:30:08 +0100

    Added missing column name for Marcinek

--- src/main/java/com/ug/cyberCafe/domain/Role.java
+++ src/main/java/com/ug/cyberCafe/domain/Role.java
@@ -1,5 +1,6 @@
 package com.ug.cyberCafe.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +15,7 @@ import javax.persistence.NamedQuery;
 public class Role {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "IDROLE", nullable = false)
 	private long idRole;
 	
 	private String role;
