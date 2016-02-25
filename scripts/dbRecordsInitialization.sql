99a1019c4888894fd3f3d16e3a90fe48f39755a3
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2016-02-25 22:10:22 +0100

    db scripts changed

---
+++ scripts/dbRecordsInitialization.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO Role(role) VALUES('ROLE_ADMIN');
+
+INSERT INTO "PUBLIC"."USER"
+( "FIRSTNAME", "LASTNAME", "EMAIL", "ACTIVE", "DATEOFBIRTH", "NICKNAME", "LOGIN", "PASSWORD", "AVATAR", "ROLE_IDROLE" )
+VALUES ('Marcin', 'Hallman', 'mhallman94@gmail.com',true, '1994-10-06', 'Strugacki', 'admin', 'admin123',null,0)
+
+INSERT INTO News(title,body,uploadDate) VALUES('Turniej League of Legends','W najbli¿szy weekend w CyberCafe odbêdzie siê turniej League of Legends dla graczy z ca³ej Polski. Na uczestników czekaj¹ upominki a na zwyciêzców wspania³e nagrody ufundowane przez Intela. Zapraszamy!!','2016-02-12');
+
+INSERT INTO News(title,body,uploadDate) VALUES('Turniej CS:GO','W najbli¿szy weekend w CyberCafe odbêdzie siê turniej CS:GO dla graczy z ca³ej Polski. Na uczestników czekaj¹ upominki a na zwyciêzców wspania³e nagrody ufundowane przez Intela. Zapraszamy!!','2016-02-12');
+
+INSERT INTO News(title,body,uploadDate) VALUES('Rozpoczêcie dzia³alnoœci','CyberCafe, miejsce, w którym mo¿esz siê zrelaksowaæ graj¹c na konsoli i PC ze znajomymi w³aœnie rozpoczê³o dzia³alnoœæ. Zapraszamy!!','2016-02-12');
+
