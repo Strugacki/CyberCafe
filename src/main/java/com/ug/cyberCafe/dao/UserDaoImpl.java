098350eb32427046987f00e11b94f8c158c59747
Michał <dajminazwe@gmail.com>
2016-03-02 17:55:59 +0100

    Code cleaning

--- src/main/java/com/ug/cyberCafe/dao/UserDaoImpl.java
+++ src/main/java/com/ug/cyberCafe/dao/UserDaoImpl.java
@@ -7,14 +7,11 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.ug.cyberCafe.domain.Address;
 import com.ug.cyberCafe.domain.User;
 
 @Repository("userDao")
@@ -36,6 +33,7 @@ public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao {
 
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<User> getAllUsers() {
 		return sessionFactory.getCurrentSession().getNamedQuery("get.All.Users").list();
--- src/main/java/com/ug/cyberCafe/service/UserServiceImpl.java
+++ src/main/java/com/ug/cyberCafe/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.ug.cyberCafe.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
