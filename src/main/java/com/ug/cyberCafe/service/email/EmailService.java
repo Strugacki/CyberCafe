a17a5ad409215ce7aa5bf87eb02371580eefe712
Michał <dajminazwe@gmail.com>
2016-03-09 22:40:47 +0100

    Revert "Revert "Test context and case for email support created, certificate issues""
    This reverts commit caf690bede71cfc293690681bdc7b6b671b8bb14.

--- src/main/java/com/ug/cyberCafe/service/email/EmailService.java
+++ src/main/java/com/ug/cyberCafe/service/email/EmailService.java
@@ -1,11 +1,13 @@
-package com.ug.cyberCafe.service;
+package com.ug.cyberCafe.service.email;
 
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.mail.MailSender;
 	import org.springframework.mail.SimpleMailMessage;
 	import org.springframework.stereotype.Service;
+	import org.springframework.transaction.annotation.Transactional;
 	 
 	@Service("email")
+	@Transactional
 	public class EmailService {
 	 
 		@Autowired
