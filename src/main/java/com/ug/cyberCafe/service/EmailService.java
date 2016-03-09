69de3b03a36faaa8c928db3cf0d657511f634129
Michał <dajminazwe@gmail.com>
2016-03-09 22:47:53 +0100

    Fix

---
+++ src/main/java/com/ug/cyberCafe/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.ug.cyberCafe.service;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.mail.MailSender;
+	import org.springframework.mail.SimpleMailMessage;
+	import org.springframework.stereotype.Service;
+	 
+	@Service("email")
+	public class EmailService {
+	 
+		@Autowired
+		private MailSender email;
+		
+		public void sendEmail(String toAddress, String fromAddress, String subject, String msgBody) {
+	 
+			SimpleMailMessage emailMsg = new SimpleMailMessage();
+			emailMsg.setFrom(fromAddress);
+			emailMsg.setTo(toAddress);
+			emailMsg.setSubject(subject);
+			emailMsg.setText(msgBody);
+			email.send(emailMsg);
+		}
+	}
