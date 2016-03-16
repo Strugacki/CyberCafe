dcdd46d983814317d23ab1d07984b3759eec99bb
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2016-03-16 12:59:04 +0100

    AddressDaoTest works

--- src/main/java/com/ug/cyberCafe/domain/User.java
+++ src/main/java/com/ug/cyberCafe/domain/User.java
@@ -1,5 +1,6 @@
 package com.ug.cyberCafe.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
