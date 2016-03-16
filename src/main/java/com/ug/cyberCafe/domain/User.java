207a8df300e6a7492cff59b92a7140c54853baf7
Michał <dajminazwe@gmail.com>
2016-03-16 13:27:21 +0100

    Add address to user method test created and working
    It seems that everything is working at this point.

--- src/main/java/com/ug/cyberCafe/domain/User.java
+++ src/main/java/com/ug/cyberCafe/domain/User.java
@@ -1,6 +1,5 @@
 package com.ug.cyberCafe.domain;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,77 +22,74 @@ import org.hibernate.validator.constraints.NotEmpty;
 import lombok.Data;
 
 @Entity
-@Table(name="USER")
-@NamedQueries({ 
-	@NamedQuery(name = "get.All.Users", query = "Select u from User u"),
-	@NamedQuery(name = "get.User.By.Login", query = "Select u from User u where u.login = ?"),
-	@NamedQuery(name = "log.In.User", query = "Select u from User u where u.login = ? and u.password = ?"),
-	@NamedQuery(name = "get.Users.By.Role", query = "Select u from User u, Role r WHERE u.role=r.idRole AND r.role=? "),
-	@NamedQuery(name = "set.Active.User.Account", query = "Update User u Set u.active = ? Where u.idUser = ?")
-})
+@Table(name = "USER")
+@NamedQueries({ @NamedQuery(name = "get.All.Users", query = "Select u from User u"),
+		@NamedQuery(name = "get.User.By.Login", query = "Select u from User u where u.login = ?"),
+		@NamedQuery(name = "log.In.User", query = "Select u from User u where u.login = ? and u.password = ?"),
+		@NamedQuery(name = "get.Users.By.Role", query = "Select u from User u, Role r WHERE u.role=r.idRole AND r.role=? "),
+		@NamedQuery(name = "set.Active.User.Account", query = "Update User u Set u.active = ? Where u.idUser = ?") })
 @Data
 public class User {
-	
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "IDUSER", nullable = false)
 	private long idUser;
-	
+
 	@NotNull
-	@Size(min=3, max=20, message="Dozwolona długość to 3-20 znaków")
+	@Size(min = 3, max = 20, message = "Dozwolona długość to 3-20 znaków")
 	@NotEmpty(message = "Podaj imię")
 	@Column(name = "FIRSTNAME", nullable = false)
 	private String firstName;
-	
+
 	@NotNull
-	@Size(min=3, max=20, message="Dozwolona długość to 3-20 znaków")
+	@Size(min = 3, max = 20, message = "Dozwolona długość to 3-20 znaków")
 	@NotEmpty(message = "Podaj nazwisko")
 	@Column(name = "LASTNAME", nullable = false)
 	private String lastName;
-	
-	@Email(message="Podaj właściwy adres email")
+
+	@Email(message = "Podaj właściwy adres email")
 	@NotNull
 	@NotEmpty(message = "Podaj właściwy adres email")
 	@Column(name = "EMAIL", nullable = false)
 	private String email;
-	
+
 	@NotNull
 	@Column(name = "ACTIVE", nullable = false)
 	private Boolean active = true;
-	
+
 	@NotNull
 	@NotEmpty(message = "Pole nie może zostać puste 'rrrr-mm-dd' ")
-	@Pattern(regexp="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])", message="Podaj właściwą datę rrrr-mm-dd ")
+	@Pattern(regexp = "[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])", message = "Podaj właściwą datę rrrr-mm-dd ")
 	@Column(name = "DATEOFBIRTH", nullable = false)
 	private String dateOfBirth;
-	
-	
+
 	@Column(name = "NICKNAME", nullable = true)
-	private String nickname=null;
-	
+	private String nickname = null;
+
 	@NotNull
-	@Size(min=5, max=20, message="Dozwolona długość to 5-20 znaków")
+	@Size(min = 5, max = 20, message = "Dozwolona długość to 5-20 znaków")
 	@NotEmpty(message = "Podaj login")
 	@Column(name = "LOGIN", nullable = false)
 	private String login;
-	
+
 	@NotNull
-	@Size(min=6, max=20, message="Dozwolona długość to 6-20 znaków")
+	@Size(min = 6, max = 20, message = "Dozwolona długość to 6-20 znaków")
 	@NotEmpty(message = "Podaj hasło")
 	@Column(name = "PASSWORD", nullable = false)
 	private String password;
-	
+
 	@Lob
 	@Column(name = "AVATAR", nullable = true)
-	private byte[] avatar=null;
-	
+	private byte[] avatar = null;
+
 	@OneToOne
-	@JoinColumn(name="ADDRESS_IDADDRESS")
+	@JoinColumn(name = "ADDRESS_IDADDRESS")
 	private Address address;
-	
-	@ManyToOne(optional=false)
-	@JoinColumn(name="ROLE_IDROLE")
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "ROLE_IDROLE")
 	private Role role;
-//	private Set<Role> roles;
-	
+	// private Set<Role> roles;
+
 }
--- src/test/java/com/ug/cyberCafe/daoUnitTests/UserDaoTest.java
+++ src/test/java/com/ug/cyberCafe/daoUnitTests/UserDaoTest.java
@@ -14,10 +14,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ug.cyberCafe.dao.AddressDao;
 import com.ug.cyberCafe.dao.UserDao;
+import com.ug.cyberCafe.domain.Address;
 import com.ug.cyberCafe.domain.User;
 
-@ContextConfiguration(locations = { "classpath:/applicationContext-test.xml"})
+@ContextConfiguration(locations = { "classpath:/applicationContext-test.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 
 @Transactional("transactionManager")
@@ -26,45 +28,78 @@ public class UserDaoTest {
 	@Autowired
 	UserDao userDao;
 	
-	
+	@Autowired
+	AddressDao addressDao;
+
 	private String FIRSTNAME = "John";
 	private String LASTNAME = "Smith";
-	/*Initialization method that clears the news table from database and add few records before starting test method*/
-	@Before public void initialize() {
+	private String EMAIL = "test@email.com";
+	private Boolean ACTIVE = true;
+	private String DATEOFBIRTH = "1994-04-10";
+	private String NICKNAME = "Andriej";
+	private String LOGIN = "qwerty";
+	private String PASSWORD = "123456";
+	
+
+	/*
+	 * Initialization method that clears the news table from database and add
+	 * few records before starting test method
+	 */
+	@Before
+	public void initialize() {
 		userDao.deleteAllUsers();
-		
-		/*First address added*/
+
+		/* First address added */
 		User userToAdd = new User();
 		userToAdd.setFirstName(FIRSTNAME);
 		userToAdd.setLastName(LASTNAME);
+		userToAdd.setEmail(EMAIL);
+		userToAdd.setActive(ACTIVE);
+		userToAdd.setDateOfBirth(DATEOFBIRTH);
+		userToAdd.setNickname(NICKNAME);
+		userToAdd.setLogin(LOGIN);
+		userToAdd.setPassword(PASSWORD);
 		userDao.addUser(userToAdd);
-		
-		/*Second address added*/
+
+		/* Second address added */
 		User userToAdd2 = new User();
 		userToAdd2.setFirstName(FIRSTNAME);
 		userToAdd2.setLastName(LASTNAME);
+		userToAdd2.setEmail(EMAIL);
+		userToAdd2.setActive(ACTIVE);
+		userToAdd2.setDateOfBirth(DATEOFBIRTH);
+		userToAdd2.setNickname(NICKNAME);
+		userToAdd2.setLogin(LOGIN);
+		userToAdd2.setPassword(PASSWORD);
 		userDao.addUser(userToAdd2);
 	}
-	
-	/*Finishing method that clears the address table from database*/
-	@After public void finishing() {
+
+	/* Finishing method that clears the address table from database */
+	@After
+	public void finishing() {
 		userDao.deleteAllUsers();
 	}
-	
+
 	@Test
 	public void addUserCheck() {
-			
+
 		List<User> retrievedUsers = userDao.getAllUsers();
-		assertEquals(FIRSTNAME,retrievedUsers.get(0).getFirstName());
-		assertEquals(LASTNAME,retrievedUsers.get(0).getLastName());
+		assertEquals(FIRSTNAME, retrievedUsers.get(0).getFirstName());
+		assertEquals(LASTNAME, retrievedUsers.get(0).getLastName());
+		assertEquals(EMAIL, retrievedUsers.get(0).getEmail());
+		assertEquals(ACTIVE, retrievedUsers.get(0).getActive());
+		assertEquals(DATEOFBIRTH, retrievedUsers.get(0).getDateOfBirth());
+		assertEquals(NICKNAME, retrievedUsers.get(0).getNickname());
+		assertEquals(LOGIN, retrievedUsers.get(0).getLogin());
+		assertEquals(PASSWORD, retrievedUsers.get(0).getPassword());
 	}
-	
+
 	@Test
 	public void getAllUsersCheck() {
 		List<User> retrievedUsers = userDao.getAllUsers();
 		assertEquals(2, retrievedUsers.size());
 	}
-	
+
 	@Test
 	public void deleteAllUsersCheck() {
 		List<User> retrievedUsers = userDao.getAllUsers();
@@ -73,27 +108,58 @@ public class UserDaoTest {
 		List<User> retrievedUsersAfterDelete = userDao.getAllUsers();
 		assertEquals(retrievedUsersAfterDelete, new ArrayList<>(0));
 	}
-	
-	@Test 
-	public void updateNewsCheck() {
+
+	@Test
+	public void updateUserCheck() {
 		List<User> retrievedUsers = userDao.getAllUsers();
 		User userToUpdate = retrievedUsers.get(0);
-		userToUpdate.setFirstName("Something else");
-		userToUpdate.setLastName("1-1-1");
+		userToUpdate.setFirstName("Michal");
+		userToUpdate.setLastName("Gorski");
+		userToUpdate.setEmail("Michal@gmail.com");
+		userToUpdate.setActive(false);
+		userToUpdate.setDateOfBirth("1995-10-10");
+		userToUpdate.setNickname("Gorsky");
+		userToUpdate.setLogin("DajMiNazwe");
+		userToUpdate.setPassword("654321");
 		userDao.updateUser(userToUpdate);
-		
+
 		List<User> retrievedUsersAfterUpdate = userDao.getAllUsers();
-		assertEquals("Sierakowice",retrievedUsersAfterUpdate.get(0).getFirstName());
-		assertEquals("20-200", retrievedUsersAfterUpdate.get(0).getLastName());
+		assertEquals("Michal", retrievedUsersAfterUpdate.get(0).getFirstName());
+		assertEquals("Gorski", retrievedUsersAfterUpdate.get(0).getLastName());
+		assertEquals("Michal@gmail.com", retrievedUsersAfterUpdate.get(0).getEmail());
+		assertEquals(false, retrievedUsersAfterUpdate.get(0).getActive());
+		assertEquals("1995-10-10", retrievedUsersAfterUpdate.get(0).getDateOfBirth());
+		assertEquals("Gorsky", retrievedUsersAfterUpdate.get(0).getNickname());
+		assertEquals("DajMiNazwe", retrievedUsersAfterUpdate.get(0).getLogin());
+		assertEquals("654321", retrievedUsersAfterUpdate.get(0).getPassword());
 	}
-	
+
 	@Test
-	public void getNewsByIdCheck() {
+	public void getUserByIdCheck() {
 		List<User> retrievedUsers = userDao.getAllUsers();
 		User userToCompare = retrievedUsers.get(0);
 		Long idToCompare = retrievedUsers.get(0).getIdUser();
 		User userToCompare2 = userDao.getUserById(idToCompare);
-		assertEquals(userToCompare,userToCompare2);
+		assertEquals(userToCompare, userToCompare2);
+	}
+	
+	@Test
+	public void addAddressToUserCheck() {
+		List<User> retrievedUsers = userDao.getAllUsers();
+		User userToUpdate = retrievedUsers.get(0);
+		Address addressToAdd = new Address();
+		addressToAdd.setCity("Kwidzyn");
+		addressToAdd.setPostalCode("80-126");
+		addressToAdd.setStreet("Lakowa");
+		addressToAdd.setLocalNumber("14");
+		addressDao.addAddress(addressToAdd);
+		List<Address> retrievedAddresses = addressDao.getAllAddresses();
+		addressToAdd = retrievedAddresses.get(0);
+		userToUpdate.setAddress(addressToAdd);
+		userDao.updateUser(userToUpdate);
+		List<User> retrievedUsersAfterUpdate = userDao.getAllUsers();
+		User userAfterUpdate = retrievedUsersAfterUpdate.get(0);
+		assertEquals(userAfterUpdate.getAddress().getIdAddress(),addressToAdd.getIdAddress());
 	}
 
 }
